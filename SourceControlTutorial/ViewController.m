461af7e4aace0f9f4aef21cdbda0fd7e071f79c9
Alok Kumar <alokkumar@Aloks-MacBook-Pro.local>
2015-07-29 22:43:59 +0530

    tableView

--- SourceControlTutorial/ViewController.m
+++ SourceControlTutorial/ViewController.m
@@ -25,5 +25,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    //cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
 @end
