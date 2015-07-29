111915a08a3499327f059391f2e22eee6d87a9c4
Guddu Gaurav <guddugaurav@Guddus-MacBook-Pro.local>
2015-07-29 20:44:27 +0530

    Initial Commit

---
+++ SourceControlTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourceControlTutorial
+//
+//  Created by Guddu Gaurav on 29/07/15.
+//  Copyright (c) 2015 Guddu Gaurav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SourceControlTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SourceControlTutorial
+//
+//  Created by Guddu Gaurav on 29/07/15.
+//  Copyright (c) 2015 Guddu Gaurav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
