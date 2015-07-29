379531a61efaf5c7c641ffd7d43270f35961834e
Guddu Gaurav <guddugaurav@Guddus-MacBook-Pro.local>
2015-07-29 21:18:41 +0530

    1. My first commit to test .

--- SourceControlTutorial/ViewController.m
+++ SourceControlTutorial/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"push my new thing to repo");
 }
 
 - (void)didReceiveMemoryWarning {
